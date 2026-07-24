077f5e8986f825f212a26dcd34e6df4d160668df
Cap LaBuff <lucasbeef00@gmail.com>
2026-07-25 01:52:21 +0200

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace ModMod;
 
 
-[BepInPlugin("rotn.katie.mod.mod", "ModMod", "0.1.0")]
+[BepInPlugin("rotn.katie.mod.mod", "ModMod", "1.1.0")]
 public class Plugin : RiftPlugin {
     protected override void OnInit() {
         Modifier.Create("Gloom", Assets.GetSprite(SpriteType.GloomOff), Assets.GetSprite(SpriteType.GloomOn));
